f69039cb3b859bb02bba047c4dd6b79442698406
Husain2121 <husainlaliwala@gmail.com>
2025-06-18 16:40:55 -0400

    added new customer page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import React from 'react';
 function App() {
   return (
 
+
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ src/Pages/Customer.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
