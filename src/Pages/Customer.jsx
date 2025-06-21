58ecbe2b1feb7f8a3a3eda9a242593e663ef62c7
Husain <husainlaliwala@gmail.com>
2025-06-21 18:10:36 -0400

    my changes

--- src/Pages/Customer.jsx
+++ src/Pages/Customer.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-import { ArrowLeft } from "lucide-react";
-import { Link } from "react-router-dom";
 
-
-function Customer() {
-    const items = [items, setitems];
-
- const test = 22
-
-
-}
